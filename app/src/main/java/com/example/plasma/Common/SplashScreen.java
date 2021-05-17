b49847ec18a6e5632a8aa60a6b7ca93b286dfbc6
Ankit <ankit.rama.yadav@gmail.com>
2021-05-17 19:10:25 +0530

    test

---
+++ app/src/main/java/com/example/plasma/Common/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.plasma.Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.plasma.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+    }
+}
