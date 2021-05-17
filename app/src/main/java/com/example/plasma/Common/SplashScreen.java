1887624aa0ebb5fde48b6cac0567987c293107e0
Ankit <ankit.rama.yadav@gmail.com>
2021-05-17 21:18:27 +0530

    Splash Screen animation

--- app/src/main/java/com/example/plasma/Common/SplashScreen.java
+++ app/src/main/java/com/example/plasma/Common/SplashScreen.java
@@ -2,17 +2,54 @@ package com.example.plasma.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.plasma.R;
+import com.example.plasma.User.UserDashboard;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+    //variables
+    ImageView backgroundImage;
+    TextView poweredbyLine;
+
+    //Animations
+    Animation sideAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        //Hooks
+        backgroundImage = findViewById(R.id.background_image);
+        poweredbyLine = findViewById(R.id.slogan);
+
+        //Hooking Animations
+        sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+
+        //set Animation on elements
+        backgroundImage.setAnimation(sideAnim);
+        poweredbyLine.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), UserDashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
---
+++ app/src/main/java/com/example/plasma/User/UserDashboard.java
@@ -0,0 +1,16 @@
+package com.example.plasma.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.plasma.R;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
